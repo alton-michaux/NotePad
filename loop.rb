34d0ef1863ee7c4dc93a06660421ed81398a3386
Alton Michaux <alton.michaux@yahoo.com>
2021-04-05 09:20:11 -0400

    made a small tweak to loop.rb and added some whitespace to other files

--- loop.rb
+++ loop.rb
@@ -2,12 +2,13 @@ def repeat(string, times)
     fail "times must be 1 or more" if times < 1
     counter = 0
     loop do
-      print string
+      puts string
       counter += 1
+      sleep 1
       if counter == times
         break
       end
     end
 end
 
-puts repeat("cool", 7)
+puts repeat("cool", 17)
